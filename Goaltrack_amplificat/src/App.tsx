c870bbf7d5fdb8cda36b5e9dede3d03147cf80d9
alin-anton <alinanton59@gmail.com>
2026-07-14 21:52:58 +0300

    folder back

--- Goaltrack_amplificat/src/App.tsx
+++ Goaltrack_amplificat/src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  return (
+    <BrowserRouter>
+        <div className="min-h-screen">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                  
+            </Routes>
+        </div>
+    </BrowserRouter>
+  );
   
 }
 
-export default App
+export default App;

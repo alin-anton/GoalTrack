a7acc0fff9ba9321873ab020c810266fef994a1f
alin-anton <alinanton59@gmail.com>
2026-07-15 13:33:45 +0300

    test

---
+++ Goaltrack_backend/goaltrack_backend/src/main/java/com/example/goaltrack_backend/GoaltrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.goaltrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoaltrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoaltrackBackendApplication.class, args);
+    }
+
+}
